dad59985f4dad32860f1e6dd4aab25951ace227b
maxxhelfer <maxx.helfer@gmail.com>
2016-04-27 13:46:42 -0600

    Added files via upload

--- searchTree.cpp
+++ searchTree.cpp
@@ -16,93 +16,21 @@ searchTree::~searchTree()
 {
     //dtor
 }
-/*
-void searchTree::printWords(std::string){
 
-//    printWords(root);
-}
-*/
 void searchTree::printWords(std::string*& data)   {
     for(int j = 0; j<349900; j++){
                 std::cout<<data[j]<<std::endl;
             }
 }
-/*
-void searchTree::addSearchNode(std::string word){
-    int counter=0;
-    searchNode *temp = new searchNode;
-    searchNode *data = new searchNode(word);
-    temp = root;
-    data->word = word;
-    data->parent = NULL;
-    data->leftChild = NULL;
-    data->rightChild = NULL;
-
-    while(temp !=NULL){
-        data->parent = temp;
-        if(data->word.compare(temp->word)<0){
-            temp = temp->leftChild;
-            counter++;
-        }
-        else {
-            temp = temp->rightChild;
-            counter++;
-        }
-    }
-    if(data->parent == NULL){
-        root = data;
-    }
-    else if(data->word.compare(data->parent->word)<0){
-        data->parent->leftChild = data;
-        data->parent = data->parent;
-    }
-    else{
-        data->parent->rightChild = data;
-        data->parent = data->parent;
-    }
-}
-*/
 
 bool searchTree::compare(std::string word, std::string*& data){
-    //for(int i=0;i<349900;i++){
-        /*
-        if (std::end(strWords) != std::find(std::begin(strWords), std::end(strWords), word)) {
-            return false;
-        }
-        else{
-            return true;
-        }
 
-        auto it = std::find(std::begin(strWords), std::end(strWords), word);
+   /*
+found=data.find(word);
+  if (found!=std::string::npos)
+    std::cout << "'haystack' also found at: " << found << '\n';
+   */
 
-        if (it != std::end(strWords))
-        {
-        return true;
-    // found *it
-        }
-        else
-        {
-        return false;
-    // not found
-        }
-
-/*
-        else if(word.compare(strWords[i]) == 0){
-            return true;
-        }
-        else if(i=349900){
-            return false;
-        }
-        else{
-            return false;
-        }
-*/
-/*
-if(data[i].find(search, 0) != std::string::npos);
-{
-    cout << data[i]<< endl;
-}
-*/
 std::string matchString = word;
     for(int x = 0; x < 349900; x++){
         if (data[x].find(matchString, 0) != std::string::npos){
@@ -113,11 +41,7 @@ std::string matchString = word;
             return false;
     }
 }
-/*
-found=data.find(word);
-  if (found!=std::string::npos)
-    std::cout << "'haystack' also found at: " << found << '\n';
-   */
+
 
 
 
